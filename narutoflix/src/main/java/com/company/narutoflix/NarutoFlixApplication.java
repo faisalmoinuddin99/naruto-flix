49539a82c4707459cf96142af27d8cd363ae35cf
faisalmoinuddin99 <faisal25march99@gmail.com>
2021-09-11 15:54:36 +0530

    Initial commit, Spring Boot Start-kick File

---
+++ narutoflix/src/main/java/com/company/narutoflix/NarutoFlixApplication.java
@@ -0,0 +1,15 @@
+package com.company.narutoflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NarutoFlixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NarutoFlixApplication.class, args);
+
+		System.out.println("Welcome to Naruto Flix Project");
+	}
+
+}
---
+++ narutoflix/src/test/java/com/company/narutoflix/NarutoFlixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.narutoflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NarutoFlixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
