8d68e8d4df22c7f1fd7d233ebf632b4d9ef71a6b
faisalmoinuddin99 <faisal25march99@gmail.com>
2021-09-11 16:31:35 +0530

    Done with configuring Firebase to Spring Boot, also done with setting up cloud firestore database

--- narutoflix/src/main/java/com/company/narutoflix/NarutoFlixApplication.java
+++ narutoflix/src/main/java/com/company/narutoflix/NarutoFlixApplication.java
@@ -1,15 +1,42 @@
 package com.company.narutoflix;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Objects;
+
 
 @SpringBootApplication
 public class NarutoFlixApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		ClassLoader classLoader = NarutoFlixApplication.class.getClassLoader() ;
+
+//		Importing serviceAccountKey.json file
+		File file = new File(Objects.requireNonNull(classLoader.getResource("serviceAccountKey.json")).getFile()) ;
+
+		FileInputStream serviceAccount = new FileInputStream(file.getAbsolutePath()) ;
+
+
+
+//		FireBase Authorization
+		FirebaseOptions options = new FirebaseOptions.Builder()
+				.setCredentials(GoogleCredentials.fromStream(serviceAccount))
+				.setDatabaseUrl("https://naruto-flix-default-rtdb.firebaseio.com")
+				.build();
+
+		FirebaseApp.initializeApp(options);
+
 		SpringApplication.run(NarutoFlixApplication.class, args);
 
-		System.out.println("Welcome to Naruto Flix Project");
+
 	}
 
 }
